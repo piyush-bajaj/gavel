14128159eb66574d8d082502ae2af9ca812ae4ec
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2022-06-10 14:58:13 +0200

    improved comments

--- analysis/throughput_analysis.py
+++ analysis/throughput_analysis.py
@@ -3,9 +3,7 @@ import json
 import re
 
 def parse_job_type_tuple(job_type):
-    # print(job_type)
     match = re.match('\(\'(.*)\', (\d+)\)', job_type)
-    # print(match)
     if match is None:
         return None
     model = match.group(1).split('(')
@@ -37,8 +35,8 @@ if os.path.exists(throughput_file):
                             output_file.write(parse_job_type_tuple(model)[2])   #batch size of model 16, 32, etc 0 if parameter absent
                             output_file.write(',')
 
-                            if second_model == 'null' :
-                                output_file.write(str(data[worker_type][model][second_model]))
+                            if second_model == 'null' : #space sharing is off
+                                output_file.write(str(data[worker_type][model][second_model])) #throughput value
                                 output_file.write(',')
                                 output_file.write('NaN') #model Resnet-18, Transformer etc
                                 output_file.write(',')
@@ -47,8 +45,8 @@ if os.path.exists(throughput_file):
                                 output_file.write('NaN')   #batch size of model 16, 32, etc 0 if parameter absent
                                 output_file.write(',')
                                 output_file.write('NaN')
-                            else :
-                                output_file.write(str(data[worker_type][model][second_model][0]))
+                            else : #space sharing is on
+                                output_file.write(str(data[worker_type][model][second_model][0])) #throughput value for first model in space sharing
                                 output_file.write(',')
                                 output_file.write(parse_job_type_tuple(second_model)[0]) #model Resnet-18, Transformer etc
                                 output_file.write(',')
@@ -56,8 +54,9 @@ if os.path.exists(throughput_file):
                                 output_file.write(',')
                                 output_file.write(parse_job_type_tuple(second_model)[2])   #batch size of model 16, 32, etc 0 if parameter absent
                                 output_file.write(',')
-                                output_file.write(str(data[worker_type][model][second_model][1]))
+                                output_file.write(str(data[worker_type][model][second_model][1])) #throughput value for second model in space sharing
+
+                            #new line after each combination
                             output_file.write('\n')
-                            # print(data[worker_type][model][second_model])
 
 
